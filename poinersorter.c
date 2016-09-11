4f1bd83c399a5bac7ae21bcca06846157b2aed43
aj470 <aj470@scarletmail.rutgers.edu>
2016-09-11 19:48:58 -0400

    with comments and added extra quotes in the input argv

--- poinersorter.c
+++ poinersorter.c
@@ -19,33 +19,48 @@ typedef struct _LList
   int listLength;
 }LList;
 
-void extractComponent (char* input)
+void extractComponent (char* token)
 {
-  int i;
-  char * component;    
-  int pos = 0;
+  char input[strlen(token)+2];
+  int j;
+  input[0] = '"';
+  for(j=0; j<strlen(token);j++)
+    {
+        input[j+1]= token[j];
+     }
+  input[strlen(token)+1]= '"';
+
+  for(j=0; j<strlen(input);j++)
+    {
+      printf("%c \n", input[j]);
+    }
+  input[strlen(token)+1]= '"';
+ 
+  int i; //for loop variable
+  char * component;    //seperating a component from input
+  int pos = 0; // tracks the poition count as we move through each character in input.
     
-  for(i = 0; i < strlen(input)+1; i++)
+  for(i = 0; i < strlen(input)+1; i++) //for loop from starting to '\0' of the input.
     {
       //if current character is not alphabetic, allocate memory for new component
-      if((isalpha(input[i]) == 0) || input[i] == '\0')
+      if((isalpha(input[i]) == 0) || input[i] == '\0') //check to see if  input[i] is not not alpha or its last character in input.
 	{
-	  component  = (char *)malloc(sizeof(char)*(1+i-pos));
-	  if(component == NULL)
+	  component  = (char *)malloc(sizeof(char)*(1+i-pos)); //allocate space for the i-pos, for example if "hello1world" then pos = 0 initally and when i=5, it will allocate for 5 space
+	  if(component == NULL) //check if space is alocated or not
 	    {
 	      printf("Error: Unable to allocate 'component'\n");
 	      return;
 	    }
-	  else
+	  else // if allocated
 	    {
-	      memcpy(component, &input[pos], (i-pos));
-	      component[strlen(component)+1] = '\0';
-	      if(component[0]!= '\0')
+	      memcpy(component, &input[pos], (i-pos)); //copy the character from input starting at pos till i-pos to component as that is our token.
+	      component[strlen(component)+1] = '\0'; //add the null terminating character at end.
+	      if(component[0]!= '\0') // if this wasn't the end then print the characters and continue
 		{
-		  printf("%s\n", component);
+		  printf("%s\n", component); // print the token
 		}
-	      pos = i + 1;
-	      free(component);
+	      pos = i + 1; // as the current place value in input was a deliminter so we will update current pos to i+1
+	      free(component); // release the component
 	    }
 	}
     }
