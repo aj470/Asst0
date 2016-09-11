5d817a89931b213f7ac14392f3b2545311d96f35
aj470 <aj470@scarletmail.rutgers.edu>
2016-09-10 22:05:24 -0400

    Updated!

--- poinersorter.c
+++ poinersorter.c
@@ -25,51 +25,58 @@ void extractComponent (char* input)
   char * component;    
   int pos = 0;
   //component  = (char *)malloc(sizeof(char)*(strlen(input)));
-	  
-  for(i = 0; i < strlen(input); i++)
+    
+  for(i = 0; i < strlen(input)+1; i++)
     {
       //if current character is not alphabetic, allocate memory for new component
-      if((isalpha(input[i]) == 0 && i > 0) || input[i+1] == '\0')
-      {
-	
-	//printf("Here:%d %c\n",i, input[i]);
-	component  = (char *)malloc(sizeof(char)*(i-pos));
-       	if(component == NULL)
-	  {
-	    printf("Error: Unable to allocate 'component'\n");
-	    return;
-	  }
-	else
-	  {
-	    memcpy(component, &input[pos], (i-pos));
-	    printf("Else: %s\n", component);
-	    pos = i;
-	    free(component);
-
-	    /*
-	    if(strlen(component) == 0)
-	      {
-		memcpy(component, input, i);
-		printf("If: %s\n", component);
-		component = (char*)realloc(component, sizeof(char)*i);
-	      }
-	    else
-	      {
-		printf("Size: %zd\n", strlen(component));
-		//copy contents of input into component
-		memcpy(component, &input[strlen(component)], (strlen(component))-i);
-		printf("Else: %s\n", component);
-	      }
-	    //copy contents of input into component
-	    //memcpy(component, &input[strlen(component)], (strlen(component))-i);
-	    //printf("%s\n", component);
-	    // free(component);*/
-	  }
-      }
+      if((isalpha(input[i]) == 0 && i > 0) && input[i+1] != '\0')
+	{
 	  
+	  //printf("Here:%d %c\n",i, input[i]);
+	  component  = (char *)malloc(sizeof(char)*(1+i-pos));
+	  if(component == NULL)
+	    {
+	      printf("Error: Unable to allocate 'component'\n");
+	      return;
+	    }
+	  else
+	    {
+	      memcpy(component, &input[pos], (i-pos));
+	      component[strlen(component)+1] = '\0';
+	      if(component[0]!= '\0')
+	      {
+	 	printf("%s\n", component);
+	      }
+	      pos = i + 1;
+	      free(component);
+
+	      /*
+		    if(strlen(component) == 0)
+		          {
+			  memcpy(component, input, i);
+			  printf("If: %s\n", component);
+			  component = (char*)realloc(component, sizeof(char)*i);
+			        }
+				    else
+				          {
+					  printf("Size: %zd\n", strlen(component));
+					  //copy contents of input into component
+					  memcpy(component, &input[strlen(component)], (strlen(component))-i);
+					  printf("Else: %s\n", component);
+					        }
+						    //copy contents of input into component
+						        //memcpy(component, &input[strlen(component)], (strlen(component))-i);
+							    //printf("%s\n", component);
+							    // free(component);*/
+	    }
+	}
+      else
+	{
+	 
+	}
     }
 }
-	  
+  
 
 int main(int argc, char *argv[])
 {
@@ -89,4 +96,3 @@ int main(int argc, char *argv[])
 
   return 0;
 }
-
