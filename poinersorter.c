9ae615f32016879bcb23231efccca5359bf19723
ggutierrez1224 <ggutierrez1224@gmail.com>
2016-09-14 00:17:29 -0400

    Update poinersorter.c

--- poinersorter.c
+++ poinersorter.c
@@ -1,7 +1,3 @@
-/*
-******************CODE COMPLIES*********************
-*****************DIDN'T DEBUG YET*******************
-*/
 #include <string.h>
 #include <stdio.h>
 #include <ctype.h>
@@ -47,9 +43,9 @@ Node* createNode(char* add)
       return NULL;
     }
   
-  create->str =(char *)malloc(sizeof(*add));
+  create->str =(char *)malloc(strlen(add)+1);
   memcpy(create->str, add, strlen(add)); 
-  create->str[sizeof(create->str)+1] = '\0';
+  create->str[strlen(create->str)+1] = '\0';
 
   create->next = NULL;
   create->prev = NULL;
@@ -81,15 +77,66 @@ void sortComponent (LList *list, char *component)
 }
 
 
+void extractComponent (LList* list , char* token)
+{
+  char input[strlen(token)+2];
+  int j;
+  input[0] = '"';
+  for(j=0; j<strlen(token);j++)
+    {
+      input[j+1]= token[j];
+    }
+  input[strlen(token)+1]= '"';
+
+  int i; //for loop variable
+  char * component;    //seperating a component from input
+  int pos = 0; // tracks the poition count as we move through each character in input.
+  
+  for(i = 0; i < strlen(input)+1; i++) //for loop from starting to '\0' of the input.
+    {
+      //if current character is not alphabetic, allocate memory for new component
+      if((isalpha(input[i]) == 0) || input[i] == '\0') //check to see if  input[i] is not not alpha or its last character in input.
+	{
+	  component  = (char *)malloc(sizeof(char)*(1+i-pos)); //allocate space for the i-pos, for example if "hello1world" then pos = 0 initally and when i=5, it will allocate for 5 space
+	  if(component == NULL) //check if space is alocated or not
+	    {
+	      printf("Error: Unable to allocate 'component'\n");
+	      return;
+	    }
+	  else // if allocated
+	    {
+	      memcpy(component, &input[pos], (i-pos)); //copy the character from input starting at pos till i-pos to component as that is our token.
+	      component[strlen(component)+1] = '\0'; //add the null terminating character at end.
+	      if(component[0]!= '\0') // if this wasn't the end then print the characters and continue
+		{
+		  sortComponent(list, component);
+		 		   
+		  printf("%s\n", component); // print the token
+		}
+	      pos = i + 1; // as the current place value in input was a deliminter so we will update current pos to i+1
+	      free(component); // release the component
+	    }
+	}
+    }
+}
+
 int main(int argc, char *argv[])
 {
   LList *listPtr = createList();
     
-  Node * output = createNode(argv[1]);
-  
-  sortComponent(listPtr, output->str);
+  //check if user input data
+  if(argc < 2)
+    {
+      printf("Error: Invalid input.\n");
+      exit(0);
+    }
 
-  printf("%s\n", output->str);
-  
+  //call function to extract components from input
+  int i;
+  for(i = 1; i < argc; i++)
+    {
+      extractComponent(listPtr, argv[i]);
+    }
+   
   return 0;
 }
